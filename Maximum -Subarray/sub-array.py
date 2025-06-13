2182183d721d123376cc796e095ebe5024fa06c3
Ivy Atieng <146369247+Atieng@users.noreply.github.com>
2025-06-13 00:11:40 -0700

    Finish up on the code of maximum sub-array

--- Maximum -Subarray/sub-array.py
+++ Maximum -Subarray/sub-array.py
@@ -5,7 +5,17 @@ class Solution(object):
         :rtype: int
         """
         # INitializze the maximum sum so far
-        max_sum =0
+        max_sum =nums[0]
         # The outer loop of the sub array
         for i in range(len(nums)):
-            cur
+            sum_of_subarray = 0
+            for j in range(i, len(nums)):
+                sum_of_subarray = sum_of_subarray +nums[j]
+                max_sum =max(max_sum,sum_of_subarray)
+
+        return max_sum
+    
+sol = Solution()
+nums =[-2, -3, 0,4, 6,2]
+print(sol.maxSubArray(nums))
+
