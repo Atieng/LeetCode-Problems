5657548a70e890c751db59b9c8eb7b51a6099fe8
Ivy Atieng <146369247+Atieng@users.noreply.github.com>
2025-06-12 23:39:23 -0700

    Create a sub-array folder

---
+++ Maximum -Subarray/sub-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # INitializze the maximum sum so far
+        max_sum =0
+        # The outer loop of the sub array
+        for i in range(len(nums)):
+            cur
